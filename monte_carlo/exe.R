b6c71973f3a55f8e123d86ded8d4c2e55a8b952e
Angela Serra <aserra@unisa.it>
2017-07-18 16:10:02 +0200

    Update exe.R

--- monte_carlo/exe.R
+++ monte_carlo/exe.R
@@ -1,27 +1,25 @@
-library(devtools)
-source('R_functions.R')
-
-## Global paramters
-R         <- 250
+## Global parameters
+R         <- 100
+n         <- 100
 nsplits   <- 25
-p         <- 500
-cont.rate <- 0.1
 
-##  Correlation models
-CorModel      <- array(0, dim=c(p,p,2))
-## Spherical
-CorModel[,,1] <- diag(1, p)
-## Block diagonal
-p1 <-  round(p/2)
-tmp <- diag(1, p)
-tmp[1:p1, 1:p1] <- cov.ar(p1, rho = 0.7, marg.vars=NULL)
-CorModel[,,2] <- tmp
+
+library(devtools)
+source.dir <- function(path=getwd(), pattern='\\.[RrSsQq]$', trace = TRUE, ...){
+   for (i in list.files(path, pattern=pattern)) {
+      if(trace) message("Sourcing:......",i)
+      source(file.path(path, i), ...)
+   }
+   if(trace) cat("\n")
+}
+source.dir('R_functions')
 
 ## Load setups
 setups    <- read.table("setups.dat", header=TRUE, stringsAsFactors=FALSE)
 
+
 ## Do cases
-for (ncase in 1:ncol(setups)) {
+for (ncase in 1:nrow(setups)) {
    set.seed(ncase * 19051977)
    source('docase.R')
 }
